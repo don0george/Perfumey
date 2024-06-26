48d8c44197fc60d3819b947971f013bacc21671a
Don <161506766+don0george@users.noreply.github.com>
2024-06-26 17:19:01 +0530

    perfumey uploaded

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from "./Components/Navbar"
+import Search from "./Components/Search"
+import Products from "./Components/Product"
+import About from "./Components/About"
+import Footer from "./Components/Footer"
+
+function App() {
+    return (
+        <div>
+
+            <Navbar></Navbar>
+            <Search></Search>
+            <Products></Products>
+            <About></About>
+            <Footer></Footer>
+
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+
+// Navbar
+
+function Navbar() {
+    return (
+      <nav>
+        <h1>Perfumey</h1>
+        <ul>
+          <li>Home  |</li>
+          <li>Produt  |</li>
+          <li>About  |</li>
+          <li>Contact Us  |</li>
+        </ul>
+      </nav>
+    )
+  }
+
+  export default Navbar
